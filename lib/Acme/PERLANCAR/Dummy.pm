102f1e6d0e0054c3d4bad9a02b7c8f010583d411
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-21 06:34:22 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Dummy.pm
@@ -0,0 +1,9 @@
+package Acme::PERLANCAR::Dummy;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Dummy distribution for various testing
---
+++ lib/Acme/PERLANCAR/Dummy/DistZilla/InsertCommandOutput.pm
@@ -0,0 +1,14 @@
+package Acme::PERLANCAR::Dummy::DistZilla::InsertCommandOutput;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+
+=encoding utf8
+
+=head1 DESCRIPTION
+
+# COMMAND: /home/u1/coba/pl/unicode/i-heart-ny.pl
