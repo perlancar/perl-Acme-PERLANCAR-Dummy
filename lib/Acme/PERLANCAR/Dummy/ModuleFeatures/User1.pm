e339c1f6b643fec46702e4739801958b4da1d3b0
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-22 11:24:27 +0700

    Add module: Acme::PERLANCAR::Dummy::ModuleFeatures::User1 for testing Module::Features & Module::Features::Dummy

---
+++ lib/Acme/PERLANCAR/Dummy/ModuleFeatures/User1.pm
@@ -0,0 +1,24 @@
+package Acme::PERLANCAR::Dummy::ModuleFeatures::User1;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use warnings;
+
+our %FEATURES = (
+    Dummy => {
+        feature2 => 1,
+        feature3 => 'a',
+    },
+);
+
+1;
+# ABSTRACT: A user module for Module::Features::Dummy
+
+=head1 DESCRIPTION
+
+This is a dummy module for testing. It declares features from the
+L<Dummy|Module::Features::Dummy> feature set.
