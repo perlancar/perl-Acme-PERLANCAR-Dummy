57f0e2c3727702345b4a54339352b733c0931278
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-26 08:30:16 +0700

    Add module: :POD::LinkToSection

---
+++ lib/Acme/PERLANCAR/Dummy/POD/LinkToSection.pm
@@ -0,0 +1,130 @@
+package Acme::PERLANCAR::Dummy::POD::LinkToSection;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Testing POD section links
+
+=head1 LINK TARGETS
+
+=head2 Subheading 1
+
+This is subheading 1.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
+
+=head2 Subheading 2
+
+This is subheading 2.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
+
+=head2 L<Foo::Bar>
+
+This is subheading 2.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
+
+=head2 List items
+
+=over
+
+=item * Baz
+
+=item * Qux
+
+=item * L<Quux::Corge>
+
+=back
+
+
+=head1 LINKS
+
+Link to section Subheading 1 (a =head2): L</Subheading 1>.
+
+Link to section Subheading 2 (a =head2): L</Subheading 2>.
+
+Link to section Foo::Bar (a =head2 but text is inside a link): L</Foo::Bar>.
+
+Link to section Baz (an =item): L</Baz>.
