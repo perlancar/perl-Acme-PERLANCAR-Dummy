2462b8e7d864336f3bf59c2d32cf3775cff65b3d
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-26 08:43:13 +0700

    More tests for :POD::LinkToSection

--- lib/Acme/PERLANCAR/Dummy/POD/LinkToSection.pm
+++ lib/Acme/PERLANCAR/Dummy/POD/LinkToSection.pm
@@ -112,11 +112,134 @@ Paragraph 15.
 
 =item * Baz
 
+This is list item 1.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
+
 =item * Qux
+
+This is list item 2.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
 
 =item * L<Quux::Corge>
 
+This is list item 3.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
+
 =back
+
+
+=head1 SOME SECTION
+
+This is a filler section.
+
+Paragraph 2.
+
+Paragraph 3.
+
+Paragraph 4.
+
+Paragraph 5.
+
+Paragraph 6.
+
+Paragraph 7.
+
+Paragraph 8.
+
+Paragraph 9.
+
+Paragraph 10.
+
+Paragraph 11.
+
+Paragraph 12.
+
+Paragraph 13.
+
+Paragraph 14.
+
+Paragraph 15.
 
 
 =head1 LINKS
@@ -125,6 +248,21 @@ Link to section Subheading 1 (a =head2): L</Subheading 1>.
 
 Link to section Subheading 2 (a =head2): L</Subheading 2>.
 
-Link to section Foo::Bar (a =head2 but text is inside a link): L</Foo::Bar>.
+Link to section Foo::Bar (a =head2 but text is inside a module link: =head2 LE<lt>Foo::BarE<gt>):
+L</Foo::Bar>. On MetaCPAN, it works.
 
-Link to section Baz (an =item): L</Baz>.
+Link to section Baz (an =item): L</Baz>. On MetaCPAN, it works.
+
+Link to section Baz (an =item but text is inside a link: =item *
+LE<lt>Quux::CorgeE<gt>): L</Quux::Corge>.
+
+
+=head1 TODO
+
+Link to section with some text inside link, e.g.:
+
+ =head2 Some L<text>
+
+Link to section with other kind of links or links with text:
+
+ =head2 Some L<text|https://example.org>
